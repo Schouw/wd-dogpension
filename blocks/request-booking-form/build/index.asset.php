e1d3ade61cd577b8645e8a22283e90b0ed361ca5
Schouw <Sabina@winther.nu>
2026-02-08 19:48:54 +0100

    Fixed minor error in booking form where the submit button was locked/disabled on error. Also a few improvements on package.json and webpack.config.cjs

---
+++ blocks/request-booking-form/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => '5739c90365b43fbd20a5');
---
+++ blocks/request-booking-form/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '14792abb3ef7e1602155');
